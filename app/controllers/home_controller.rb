9d90b461a62ad1952e6936d304d0a55ade0dae30
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-06 21:37:59 +0400

    new texts and speakers page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 class HomeController < ApplicationController
+  layout 'content'
+
+  def pay
+    render layout: 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/pls-take-my-money' => 'home#pay', as: :payment
   get '/thank-u' => 'home#thank'
+  get '/speakers' => 'home#speakers'
   root 'home#index'  
 end
