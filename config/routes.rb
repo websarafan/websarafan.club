04d433155649d45943add221715d9ce98b94a45c
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-15 18:00:56 +0400

    add schedule

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/pls-take-my-money' => 'home#pay', as: :payment
   get '/thank-u' => 'home#thank'
   get '/speakers' => 'home#speakers'
+  get '/schedule' => 'home#schedule'
   root 'home#index'  
 end
