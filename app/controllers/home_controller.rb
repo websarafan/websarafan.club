b63f176fbd991429748907d813dfdd05fb413869
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-20 14:44:53 +0400

    add crosslinking between speakers page and individual speaker page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,11 @@ class HomeController < ApplicationController
   def pay
     render layout: 'application'
   end
+
+  def speakers
+    @speakers = I18n.t('speakers').map do |name, desc|
+      name_aux = name.split.join
+      [name, desc, "speakers/#{name_aux}.jpg", "/speakers/#{I18n.transliterate(name_aux)}"]
+    end
+  end
 end
