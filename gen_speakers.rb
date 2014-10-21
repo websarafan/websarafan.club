d62569f014cf49dd623d1669401d91f58c4b5630
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-21 14:15:34 +0400

    change layout for speaker page

--- gen_speakers.rb
+++ gen_speakers.rb
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 s = I18n.t('speakers').map(&:first).inject({}) { |res, name| res[name] = I18n.transliterate(name.split.join); res  }
-# s.delete('Евгений Тупикин')
+s.delete('Евгений Тупикин')
+s.delete('Алена Муравлянская')
 
 path_to_image = ->(name) { "speakers/#{name.split.join}.jpg" }
 path_to_tpl = ->(tname) {"app/views/speakers/#{tname}.html.erb"}
 gen_tpl = ->(name, path_to_image) {
 <<END
-<div class="row">
+<div class="row speaker-details">
   <div class="large-12 columns">
-    <div class="large-8 columns">
     <h1 class="text-center">#{name}</h1>
-
+    <img src="<%= image_path('#{path_to_image}') %>" />
     <div class="text-center">
       <a href="<%=payment_path%>" class="button" role="button">Присоединяйтесь!</a>
     </div>
-
-    </div>
-    <div class="large-4 columns text-center author-column">
-      <p><img src="<%= image_path('#{path_to_image}') %>" /></p>
-    </div>    
   </div>
 </div>
 END
@@ -26,7 +21,7 @@ END
 
 require 'pathname'
 s.each do |(name, tname)|
-  unless (f = Pathname.new(path_to_tpl.call(tname))).exist?
+  if (f = Pathname.new(path_to_tpl.call(tname))).exist?
     f.write(gen_tpl.call(name, path_to_image.call(name)))
   end
 end
