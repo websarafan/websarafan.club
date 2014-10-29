8afafd03904b490b03c070f0e36d5ec6c975d178
Eugene Hlyzov <hlyzov@gmail.com>
2014-10-29 20:53:13 +0300

    data structure for schedule

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,9 +1,13 @@
 module Dictionary
-  class Speaker < Struct.new(:name, :desc, :gen_avatar_link, :gen_profile_link, :articles, :topics_headline, :topics)
+  class Speaker < Struct.new(:name, :desc, :gen_avatar_link, :gen_profile_link, :articles, :topics_headline, :topics, :facebook)
     def translit
       I18n.transliterate(self.name.split.join)
     end
   end
+
+  class Day < Struct.new(:date, :headline, :talks); end
+  class Talk < Struct.new(:row, :speaker, :time, :title, :details); end
+
 
   def self.price
     date_aux = nil
@@ -45,6 +49,26 @@ module Dictionary
         topics_headline,
         topics
       )      
+    end
+  end
+
+  def self.schedule
+    first_day_aux, *daily_tables = I18n.t('schedule')
+    first_day = Date.parse(first_day_aux)
+    daily_tables.each_with_index.map do |(headline, talks_rows), index| 
+      Day.new.tap do |day| 
+        day.date = first_day + index        
+        day.headline = headline
+        day.talks = talks_rows.map do |talk_row| 
+          Talk.new.tap do |talk| 
+            match = talk_row.match /^(?<time>\d\d\.\d\d)\ (?<title>.+?)\ \((?<name>.+)\)$/
+            talk.row = talk_row
+            talk.time = match[:time]
+            talk.title = match[:title]              
+            talk.speaker = Query[:speakers].find { |speaker| speaker.name == match[:name] }
+          end
+        end.select(&:speaker)
+      end
     end
   end
 end
